f72ba4f440638742eb01d0526171487ca21b7edd
v-flores <victoralejandroflores88@gmail.com>
2019-02-12 19:32:27 -0600

    JS Objects Assignment

--- 01-javascript/02-js-fundamentals/01-js-objects.js
+++ 01-javascript/02-js-fundamentals/01-js-objects.js
@@ -1,1 +1,36 @@
 // 30 minutes max
+let students = [
+    {name: 'Remy', cohort: 'Jan'},
+    {name: 'Genevieve', cohort: 'March'},
+    {name: 'Chuck', cohort: 'Jan'},
+    {name: 'Osmund', cohort: 'June'},
+    {name: 'Nikki', cohort: 'June'},
+    {name: 'Boris', cohort: 'June'}
+];
+
+console.log(students);
+
+
+let users = {
+    employees: [
+        {'first_name':  'Miguel', 'last_name' : 'Jones'},
+        {'first_name' : 'Ernie', 'last_name' : 'Bertson'},
+        {'first_name' : 'Nora', 'last_name' : 'Lu'},
+        {'first_name' : 'Sally', 'last_name' : 'Barkyoumb'}
+    ],
+    managers: [
+       {'first_name' : 'Lillian', 'last_name' : 'Chambers'},
+       {'first_name' : 'Gordon', 'last_name' : 'Poe'}
+    ]
+ };
+
+
+ for(let type in users) {
+    console.log(type);
+    var count = 1;
+    for(let person in users[type]) {
+      let name = users[type][person].first_name+users[type][person].last_name;
+      console.log(`${count} - ${users[type][person].last_name}, ${users[type][person].first_name} - ${name.length}`);
+      count++;
+    }
+ }
